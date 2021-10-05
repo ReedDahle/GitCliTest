346b1da000dc8d371dda653ad72435b4ea2005e0
Reed Dahle <rexius923@gmail.com>
2021-10-05 15:53:07 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GitCliTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from git!");
+            Console.WriteLine("Say something nice: ");
+            string input = Console.ReadLine();
+            Console.WriteLine($"You said: {input}");
+        }
+    }
+}
